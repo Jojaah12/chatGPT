cd09c8bd4cc52bfe921a14561af419fab156d347
Jojaah12 <david.aljojah@gmail.com>
2023-03-18 14:22:54 +0100

    created messafe form and added to chat component

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -6,6 +6,7 @@ import {
 } from 'react-chat-engine-advanced';
 
 import Header from '@/components/customHeader';
+import StandardMessageForm from '@/components/customMessageForms/StandardMessageForm';
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(
@@ -20,7 +21,19 @@ const Chat = () => {
       <MultiChatWindow
         {...chatProps}
         style={{ height: '100vh' }}
-        renderChatHeader={(chat) => <Header chat={chat} />}
+        renderChatHeader={(chat) => (
+          <Header
+            chat={chat}
+            renderMessageForm={(props) => {
+              return (
+                <StandardMessageForm
+                  props={props}
+                  activeChat={chatProps.chat}
+                />
+              );
+            }}
+          />
+        )}
       />
     </div>
   );
