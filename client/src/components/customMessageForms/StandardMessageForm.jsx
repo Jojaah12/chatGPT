2fdd183b59c292564018f74baa382da0859232c2
Jojaah12 <david.aljojah@gmail.com>
2023-03-18 20:49:30 +0100

    finsihed designing input field

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,7 +1,45 @@
-import React from 'react';
+import { XMarkIcon } from '@heroicons/react/24/solid';
+import React, { useState } from 'react';
 
 const StandardMessageForm = () => {
-  return <div>StandardMessageForm</div>;
+  const [message, setMessage] = useState('');
+  const [attachment, setAtachment] = useState('');
+  const [preview, setPreview] = useState('');
+
+  const handleChange = (e) => setMessage(e.target.value);
+
+  return (
+    <div className="message-form-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img
+            className="message-form-preview-image"
+            src={preview}
+            onLoad={() => URL.revokeObjectURL(preview)}
+            alt="preview"
+          />
+          <XMarkIcon
+            className="message-form-icon-x"
+            onClick={() => {
+              setPreview('');
+              setAtachment('');
+            }}
+          />
+        </div>
+      )}
+      <div className="message-form">
+        <div className="message-form-input-container">
+          <input
+            className="message-form-input"
+            type="text"
+            value={message}
+            onChange={handleChange}
+            placeholder="send a message...."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StandardMessageForm;
